ab2df5da46d807428eabdde9893888c9f461ecfc
Tymo <39733374+Strike2610@users.noreply.github.com>
2023-08-07 15:11:55 +0200

    Remove API dependence

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ async function appStart() {
 			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	}).addTo(map);
 
-	// await addMarkers(WORKERS, blueMarker, map);
-	// await addMarkers(OFFICES, greenMarker, map); // TODO: get official test data
-	// await addMarkers(WORKSITES, redMarker, map); // TODO: get official test data
+	await addMarkers(WORKERS, blueMarker, map);
+	// await addMarkers(OFFICES, greenMarker, map);
+	// await addMarkers(WORKSITES, redMarker, map);
 }
 
 async function addMarkers(fileLocation, marker, map) {
@@ -36,50 +36,7 @@ async function addMarkers(fileLocation, marker, map) {
 		readFile = await file.text(),
 		lines = readFile.split(/\r?\n/);
 
-	for (let i = 0; i < lines.length; i++) {
-		setTimeout(save(getLatLon(lines[i])), 1500 * i);
+	for (const line of lines) {
+		L.marker(line.split(', '), { icon: marker }).addTo(map);
 	}
-	// const promises = [];
-	// for (const line of lines) {
-	// 	promises.push(
-	// 		new Promise(async (resolve, reject) => {
-	// 			const location = await getMarkerLocation(line);
-	// 			if (location) return resolve(location);
-	// 			return reject();
-	// 		})
-	// 	);
-	// }
-
-	// for (const location of await Promise.all(promises)) {
-	// 	L.marker(location, { icon: marker }).addTo(map);
-	// }
-}
-
-// async function getMarkerLocation(address) {
-// 	let items = address.split(',');
-// 	let locationUrl = `http://localhost:8080/?format=json&country=netherlands&city=${
-// 		items[0]
-// 	}&street=${items[2] + '%20' + items[1]}`;
-// 	let location;
-// 	let returnData = await fetch(locationUrl);
-// 	if (!returnData.ok) console.log('Something went wrong!');
-// 	else location = await returnData.json();
-// 	console.log(locationUrl);
-// 	return [location[0].lat, location[0].lon];
-// }
-
-async function getLatLon(address) {
-	let items = address.split(',');
-	let url = `https://nominatim.openstreetmap.org/?format=json&country=netherlands&city=${
-		items[0]
-	}&street=${items[2] + ' ' + items[1]}`;
-	const result = await fetch(url).then((response) => {
-		if (!response.ok) console.log('Something went wrong!');
-		else return response.json();
-	});
-	return [result[0].lat, result[0].lon];
-}
-
-function save(location) {
-	// TODO: store retrieved to cache to avoid lockout
 }
